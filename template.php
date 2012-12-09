aea888280332c081b9816e243ca07029757ff5be
Nathaniel Hoag <nathaniel.hoag@acquia.com>
2012-12-09 15:10:10 -0500

    Removes page title from front page.

--- template.php
+++ template.php
@@ -146,11 +146,11 @@ function cask_preprocess_html(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("page" in this case.)
  */
-/* -- Delete this line if you want to use this function
 function cask_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+  if ($variables['is_front']) {
+    $variables['title'] = '';
+  }
 }
-// */
 
 /**
  * Override or insert variables into the node templates.
